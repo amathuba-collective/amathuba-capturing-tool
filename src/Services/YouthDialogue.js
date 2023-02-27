8c4f6f99b9287629450f021a353c664630e14c52
Niyaaz-Majiet <niyaaz.majiet@rank.com>
2023-02-27 21:56:23 +0200

    Updated urls ofr cors fix

--- src/Services/YouthDialogue.js
+++ src/Services/YouthDialogue.js
@@ -1,25 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const saveYouthDialogue = (data) => {
-    return new Promise((resolve => {
-        axios.post('https://amabe-env.eba-5iepkthj.us-east-1.elasticbeanstalk.com/chatYouth', data)
-            .then(response => {
-                resolve(response)
-            })
-            .catch((err) => {
-                resolve({error: err})
-            });
-    }));
-}
+  return new Promise((resolve) => {
+    axios
+      .post("https://api.amathuba-collective.live/chatYouth", data)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((err) => {
+        resolve({ error: err });
+      });
+  });
+};
 
 export const getRecentYouthDialogueData = () => {
-    return new Promise((resolve => {
-        axios.get('https://amabe-env.eba-5iepkthj.us-east-1.elasticbeanstalk.com/chatYouth')
-            .then(response => {
-                resolve(response)
-            })
-            .catch((err) => {
-                resolve({error: err})
-            });
-    }));
-}
+  return new Promise((resolve) => {
+    axios
+      .get("https://api.amathuba-collective.live/chatYouth")
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((err) => {
+        resolve({ error: err });
+      });
+  });
+};
